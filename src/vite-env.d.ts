9cd855a2e4f35489f09fd2920d849e5183da6cf1
Eldrex Delos Reyes Bula <eldrexdelosreyesbula@gmail.com>
2026-03-19 17:57:44 +0800

    Add files via upload

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite-plugin-pwa/client" />
+
 interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
@@ -5,6 +7,7 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_VAPID_KEY: string;
 }
 
 interface ImportMeta {
